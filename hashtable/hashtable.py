4febc7da37d1bae3c8747440c540687d067125cd
Jaytee <Padilla.Jaytee@gmail.com>
2020-06-01 20:32:49 -0700

    Implemented djb2 hashing function

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -22,6 +22,7 @@ class HashTable:
 
     def __init__(self, capacity):
         # Your code here
+        pass
 
 
     def get_num_slots(self):
@@ -35,6 +36,7 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        pass
 
 
     def get_load_factor(self):
@@ -44,6 +46,7 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        pass
 
 
     def fnv1(self, key):
@@ -54,6 +57,7 @@ class HashTable:
         """
 
         # Your code here
+        pass
 
 
     def djb2(self, key):
@@ -63,6 +67,10 @@ class HashTable:
         Implement this, and/or FNV-1.
         """
         # Your code here
+        hash = 5381
+        for c in key:
+            hash = (hash * 33) + ord(c)
+        return hash
 
 
     def hash_index(self, key):
@@ -82,6 +90,8 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        slot = hash_index(key)
+        
 
 
     def delete(self, key):
@@ -93,6 +103,7 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        pass
 
 
     def get(self, key):
@@ -104,6 +115,7 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        pass
 
 
     def resize(self, new_capacity):
@@ -114,6 +126,7 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        pass
 
 
 
